89b63067ca850e5714939b0b1faf88498181e1ff
Benoit Jacob <benoitjacob@google.com>
2019-04-09 10:14:26 -0400

    Internal change
    PiperOrigin-RevId: 242661071

--- tune.h
+++ tune.h
@@ -1,3 +1,59 @@
+// Library doing minimal CPU detection to decide what to tune asm code for.
+//
+// # Tuning vs Path
+//
+// Tunings are merely local variations of optimized code paths, that are
+// drop-in replacements for each other --- the input and output data layouts
+// are identical.  By contrast, what ruy calls a Path dictates its own
+// data layouts. For example, Path::kNeonDotprodAsm will use different
+// layouts compared to Path::kNeonAsm; but within each, different tunings
+// will share that same layout.
+//
+// # Tuning is for now only based on 1 bit: OutOfOrder / InOrder
+//
+// In practice, each of our asm code paths only needs one bit information to
+// decide on tuning: whether the CPU is out-of-order or in-order.
+// That is because out-of-order CPUs are by definition relatively insensitive
+// to small-scale asm details (which is what "tuning" is about); and for each
+// asm code path, there tends to be one main in-order CPU architecture that
+// we focus our tuning effort on. Examples:
+//  * For Path::kNeonAsm, the main in-order CPU is Cortex-A53/A55 (pre-dotprod)
+//  * For Path::kNeonDotprodAsm, the main in-order CPU is Cortex-A55r1 (dotprod)
+//
+// Because having tuned code paths is a compromise of efficiency gains
+// versus implementation effort and code size, we are happy to stop at just this
+// single bit of information, OutOfOrder/InOrder, at least in the current CPU
+// landscape. This could change in the future.
+//
+// # Implementation notes and alternatives.
+//
+// The current implementation uses a nano-benchmark, see tune.cc.
+// That is why it's quite expensive, making caching /
+// statefulness necessary (see TuningResolver class comment).
+//
+// An interesting alternative, which was explained to us by Marat Dukhan
+// (maratek@) after this was implemented, would be to use the
+// getcpu(2) system call on Linux. This returns a
+// numeric CPU identifier that could be mapped to a OutOfOrder/InOrder
+// classification given additional information about the CPU.  Such
+// additional information could be obtained by the cpuinfo library,
+//   https://github.com/pytorch/cpuinfo
+// which obtains this information mainly from parsing /proc/cpuinfo.
+// Pros:
+//   * Would remove the need for the relatively expensive nano-benchmark
+//     (dozens of microseconds, which have to be reevaluated again several
+//     times per second).
+//   * Would conceivably be more reliable.
+// Cons:
+//   * Linux-specific.
+//   * Modest binary size increase (Marat mentioned the cpuinfo lib is 20k).
+//   * Won't support exactly 100% of devices (nonstandard /proc/cpuinfo etc).
+//
+// We could also have both:
+//  * Maybe by trying getcpu first if supported, then falling back to a
+//    nano-benchmark.
+//  * Maybe using getcpu in conjunction with the nano-benchmark to cache
+//    per-CPU-id nano-benchmark results.
 #ifndef TENSORFLOW_LITE_EXPERIMENTAL_RUY_TUNE_H_
 #define TENSORFLOW_LITE_EXPERIMENTAL_RUY_TUNE_H_
 
@@ -5,8 +61,26 @@
 
 namespace ruy {
 
-enum class Tuning { kAuto, kOutOfOrder, kInOrder };
+enum class Tuning {
+  // kAuto means please use auto-detection. It's the default in the
+  // user-visible parts (see Context). It's meant to be resolved to an
+  // actual tuning at some point by means of TuningResolver.
+  kAuto,
+  // Target an out-order CPU. Example: ARM Cortex-A75.
+  kOutOfOrder,
+  // Target an in-order CPU. Example: ARM Cortex-A55.
+  kInOrder
+};
 
+// Why a TuningResolver class?
+//
+// Ideally, this Library would offer a single function,
+//   Tuning GetCurrentCPUTuning();
+//
+// However, determining information about the current CPU is not necessarily,
+// cheap, so we currently cache that and only invalidate/reevaluate after
+// a fixed amount of time. This need to store state is why this library
+// has to expose a class, TuningResolver, not just a function.
 class TuningResolver {
  public:
   TuningResolver() {}
---
+++ tune_test.cc
@@ -0,0 +1,35 @@
+#include "tune.h"
+
+#include <cstdlib>
+
+#include "testing/base/public/gunit.h"
+
+namespace ruy {
+namespace {
+
+TEST(TuneTest, TuneTest) {
+  TuningResolver tuning_resolver;
+  ASSERT_FALSE(tuning_resolver.Resolve() == Tuning::kAuto);
+  // 1 second is likely higher than TuningResolver's internal cache expiry,
+  // exercising the logic invalidating earlier tuning resolutions.
+  sleep(1);
+  ASSERT_FALSE(tuning_resolver.Resolve() == Tuning::kAuto);
+
+  tuning_resolver.SetExplicitTuning(Tuning::kAuto);
+
+  for (auto tuning : {Tuning::kOutOfOrder, Tuning::kInOrder}) {
+    tuning_resolver.SetExplicitTuning(tuning);
+    ASSERT_TRUE(tuning_resolver.Resolve() == tuning);
+    // See above comment about 1 second.
+    sleep(1);
+    ASSERT_TRUE(tuning_resolver.Resolve() == tuning);
+  }
+}
+
+}  // namespace
+}  // namespace ruy
+
+int main(int argc, char** argv) {
+  ::testing::InitGoogleTest(&argc, argv);
+  return RUN_ALL_TESTS();
+}
