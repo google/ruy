13689fece39438b48d154cadcfc467c05ca83447
Jared Duke <jdduke@google.com>
2019-04-16 16:22:07 -0400

    Opensource Ruy
    PiperOrigin-RevId: 243866907

--- test.h
+++ test.h
@@ -1,3 +1,18 @@
+/* Copyright 2019 Google LLC. All Rights Reserved.
+
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+
+    http://www.apache.org/licenses/LICENSE-2.0
+
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
+==============================================================================*/
+
 #ifndef TENSORFLOW_LITE_EXPERIMENTAL_RUY_TEST_H_
 #define TENSORFLOW_LITE_EXPERIMENTAL_RUY_TEST_H_
 
@@ -466,12 +481,14 @@ Context& GlobalContext() {
   return context;
 }
 
-#if defined __has_feature && __has_feature(thread_sanitizer)
+#if defined(__has_feature)
+#if __has_feature(thread_sanitizer)
 #define RUY_TSAN
 #endif
-#if defined __has_feature && __has_feature(address_sanitizer)
+#if __has_feature(address_sanitizer)
 #define RUY_ASAN
 #endif
+#endif  // defined(__has_feature)
 
 template <typename LhsScalar, typename RhsScalar, typename DstScalar,
           typename Spec>
