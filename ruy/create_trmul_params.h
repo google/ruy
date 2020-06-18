b7649fa263e550ee83cf190cb4830382969ad0aa
Benoit Jacob <benoitjacob@google.com>
2020-06-18 11:40:22 -0700

    Refactoring of the front-end code.
    This is largely about splitting dispatch.h into smaller libraries, emphasizing the divide between the templatized code up to CreateTrMulParams and the detemplatized code downstream from there, moving code to .cc files reducing the amount of internal code #included by ruy.h, and changing some terminology:
    Dispatch --> FrontEnd
    EnforceSupport --> Validate
    
    In one place this also moves some code out of middle-end (TrMul) into front-end: the allocation of the packed matrix buffers used to be done in TrMul itself. Now with the prepacked-matrix-caching feature done in he front-end, it makes more sense to move that there too, which resulted in PreparePackedMatrices.
    
    PiperOrigin-RevId: 317146687

---
+++ ruy/create_trmul_params.h
@@ -0,0 +1,286 @@
+/* Copyright 2020 Google LLC. All Rights Reserved.
+
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+
+    http://www.apache.org/licenses/LICENSE-2.0
+
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
+==============================================================================*/
+
+// Implementation of CreateTrMulParams, see function comment.
+
+#ifndef RUY_RUY_CREATE_TRMUL_PARAMS_H_
+#define RUY_RUY_CREATE_TRMUL_PARAMS_H_
+
+#include <type_traits>
+
+#include "ruy/ctx.h"
+#include "ruy/kernel.h"
+#include "ruy/mat.h"
+#include "ruy/mul_params.h"
+#include "ruy/pack.h"
+#include "ruy/path.h"
+#include "ruy/trmul_params.h"
+
+namespace ruy {
+// While the only entry point to this file is CreateTrMulParams, its templatized
+// nature requires putting more code in this header than we would like. This
+// internal implementation code is enclosed in namespace 'detail'.
+namespace detail {
+
+void CreatePackedLayout(const MatLayout& src, const Type& scalar,
+                        const KernelLayout& kernel_layout, PMatLayout* packed);
+
+template <typename Scalar, typename PackedScalar>
+void CreatePackedMatrix(Side side, const KernelLayout& kernel_layout,
+                        TrMulParams* params) {
+  // Ruy always uses 32-bit signed accumulators for quantized
+  // matrix multiplication, so we would like to always use std::int32_t
+  // unconditionally for SumsType.
+  // However, for floating point types, we still need a reasonable type here to
+  // avoid tripping assertions elsewhere in the code.
+  using SumsType =
+      typename std::conditional<std::is_floating_point<Scalar>::value, Scalar,
+                                std::int32_t>::type;
+
+  const EMat& src = params->src[side];
+  PEMat* packed = &params->packed[side];
+  packed->data_type = Type::Create<PackedScalar>();
+  packed->sums_type = Type::Create<SumsType>();
+  CreatePackedLayout(src.layout, packed->data_type, kernel_layout,
+                     &packed->layout);
+  packed->zero_point = Pack<PackedScalar, Scalar>(src.zero_point);
+}
+
+inline bool IsColMajorTrMul(const TrMulParams& params) {
+  return IsColMajor(params.src[Side::kLhs].layout) &&
+         IsColMajor(params.src[Side::kRhs].layout) &&
+         IsColMajor(params.dst.layout);
+}
+
+template <typename KernelType>
+struct CheckKernelPathImpl {
+  static void Run(Path) {
+    // Do nothing.
+    // Path fallbacks are normal in general (see RUY_INHERIT_KERNEL).
+    // That is to say that one may instantiate ruy::Mul with a weird combination
+    // of types, such as LhsScalar==float and RhsScalar==double, and have it
+    // work by silently falling back to Path::kStandardCpp. Only in specific
+    // cases do we have dedicated kernels overriding that fallback, and that is
+    // what partial specializations of this template will check.
+  }
+};
+
+#if RUY_DCHECK_IS_ENABLED
+template <Path ThePath, typename SrcScalar, typename AccumScalar,
+          typename DstScalar>
+struct CheckKernelPathImpl<Kernel<ThePath, SrcScalar, SrcScalar, DstScalar,
+                                  MulParams<AccumScalar, DstScalar>>>
+    final {
+  using KernelType = Kernel<ThePath, SrcScalar, SrcScalar, DstScalar,
+                            MulParams<AccumScalar, DstScalar>>;
+  static void Run(Path expected_path) {
+    // We want to assert that we are using a dedicated Kernel specialization and
+    // not a fallback when we know we are in a case where such a kernel
+    // specialization exists. At the moment in the current state of ruy's
+    // architecture support for ARM and x86, that is when LhsScalar==RhsScalar
+    // (already implied in this partial specialization) and when that type is
+    // either float, int8, or uint8. Indeed, we have kernels supporting float
+    // and int8, and we have the packing code converting uint8 to int8 (see
+    // PackedTypeImpl).
+    static constexpr bool kSrcScalarTypeSupportsFastKernels =
+        std::is_same<SrcScalar, float>::value ||
+        std::is_same<SrcScalar, std::int8_t>::value ||
+        std::is_same<SrcScalar, std::uint8_t>::value;
+    if (kSrcScalarTypeSupportsFastKernels) {
+      RUY_DCHECK_EQ(expected_path, KernelType::kPath);
+    }
+  }
+};
+#endif
+
+template <typename KernelType>
+void CheckKernelPath(Path expected_path) {
+  CheckKernelPathImpl<KernelType>::Run(expected_path);
+}
+
+template <Path ThePath, typename LhsScalar, typename RhsScalar,
+          typename DstScalar, typename MulParamsType>
+void PopulateTrMulParams(TrMulParams* params) {
+  // The optimized code paths don't handle the full generality of Ruy's API.
+  // Fall back to Path::kStandardCpp if necessary.
+  bool fallback_to_standard_cpp = false;
+  if (ThePath != Path::kStandardCpp) {
+    // The optimized code paths currently only handle the case of all matrices
+    // being column major.
+    if (!IsColMajorTrMul(*params)) {
+      fallback_to_standard_cpp = true;
+    }
+  }
+
+  if (fallback_to_standard_cpp) {
+    PopulateTrMulParams<Path::kStandardCpp, LhsScalar, RhsScalar, DstScalar,
+                        MulParamsType>(params);
+    return;
+  }
+
+  using PackedLhsScalar = PackedType<ThePath, LhsScalar>;
+  using PackedRhsScalar = PackedType<ThePath, RhsScalar>;
+  using Kernel = Kernel<ThePath, PackedLhsScalar, PackedRhsScalar, DstScalar,
+                        MulParamsType>;
+  using LhsKernelLayout = typename Kernel::LhsLayout;
+  using RhsKernelLayout = typename Kernel::RhsLayout;
+
+  params->path = ThePath;
+
+  CreatePackedMatrix<LhsScalar, PackedLhsScalar>(
+      Side::kLhs, ToKernelLayout<LhsKernelLayout>(), params);
+  CreatePackedMatrix<RhsScalar, PackedRhsScalar>(
+      Side::kRhs, ToKernelLayout<RhsKernelLayout>(), params);
+  params->run_pack[Side::kLhs] =
+      &RunPack<ThePath, LhsKernelLayout, LhsScalar, PackedLhsScalar>;
+  params->run_pack[Side::kRhs] =
+      &RunPack<ThePath, RhsKernelLayout, RhsScalar, PackedRhsScalar>;
+  params->run_kernel = &RunKernel<Kernel>::Run;
+  CheckKernelPath<Kernel>(ThePath);
+}
+
+// PopulateTrMulParamsAllCompiledPaths calls into one of multiple
+// instantiations of PopulateTrMulParams. For each bit that is set in
+// CompiledPaths, it statically instantiates PopulateTrMulParams with a Path
+// corresponding to that single bit. The call to PopulateTrMulParams is
+// guarded by a runtime check that it is in fact the dynamically selected path.
+//
+// PopulateTrMulParamsAllCompiledPaths is implemented with template
+// metaprogramming by mutual recursion between PathSearchCountdown and
+// PathSearchCompiledPaths.
+//
+// PopulateTrMulParamsAllCompiledPaths is logically implementing the following
+// computation:
+//
+// template <Path CompiledPaths>
+// void PopulateTrMulParamsAllCompiledPaths(Path the_path,
+//                                            TrMulParams* params) {
+//   for (int bit = 8 * sizeof(Path) - 1; bit != -1; bit--) { // [1]
+//     Path current_path = static_cast<Path>(1 << bit);
+//     if ((CompiledPaths & current_path) != Path::kNone) { // [2]
+//       if (current_path == the_path) { // [3]
+//         PopulateTrMulParams<current_path, ...>(the_path, params);
+//         return;
+//       }
+//     }
+//   }
+// }
+//
+//
+//
+// [1] - Done by the main definition of PathSearchCountdown. The `bit--` is
+// done in the recursion of PathSearchOnlyCompiledPaths.
+// [2] - Done by PathSearchOnlyCompiledPaths's partial template
+// specialization on InCompiledPaths. This is the check which necessitates
+// doing the whole computation at C++ compile time.
+// [3] - Done by the `if` in the main definition of
+// PathSearchOnlyCompiledPaths.
+//
+// The template metaprogramming is necessary because:
+// - In `PopulateTrMulParams<current_path, ...>`, current_path must be a C++
+// compile-time constant.
+// - PopulateTrMulParamsAllCompiledPaths must not instantiate
+// inner loops for paths that are not in CompiledPaths, since that can result in
+// bogus instantiations which cause a compile time failure.
+template <Path CompiledPaths, int BitNumber, typename LhsScalar,
+          typename RhsScalar, typename DstScalar, typename MulParamsType>
+struct PathSearchCountdown;
+
+template <Path CompiledPaths, bool InCompiledPaths, int BitNumber,
+          typename LhsScalar, typename RhsScalar, typename DstScalar,
+          typename MulParamsType>
+struct PathSearchOnlyCompiledPaths {
+  static constexpr Path kCurrentPath = static_cast<Path>(1 << BitNumber);
+  static void Search(Path the_path, TrMulParams* params) {
+    if (kCurrentPath == the_path) {
+      PopulateTrMulParams<kCurrentPath, LhsScalar, RhsScalar, DstScalar,
+                          MulParamsType>(params);
+      return;
+    }
+    PathSearchCountdown<CompiledPaths, BitNumber - 1, LhsScalar, RhsScalar,
+                        DstScalar, MulParamsType>::Search(the_path, params);
+  }
+};
+
+// Skip this iteration if CompiledPaths doesn't contain the specified path.
+template <Path CompiledPaths, int BitNumber, typename LhsScalar,
+          typename RhsScalar, typename DstScalar, typename MulParamsType>
+struct PathSearchOnlyCompiledPaths<CompiledPaths, false, BitNumber, LhsScalar,
+                                   RhsScalar, DstScalar, MulParamsType> {
+  static void Search(Path the_path, TrMulParams* params) {
+    PathSearchCountdown<CompiledPaths, BitNumber - 1, LhsScalar, RhsScalar,
+                        DstScalar, MulParamsType>::Search(the_path, params);
+  }
+};
+
+template <Path CompiledPaths, int BitNumber, typename LhsScalar,
+          typename RhsScalar, typename DstScalar, typename MulParamsType>
+struct PathSearchCountdown {
+  static constexpr Path kCurrentPath = static_cast<Path>(1 << BitNumber);
+  static void Search(Path the_path, TrMulParams* params) {
+    PathSearchOnlyCompiledPaths<
+        CompiledPaths, (CompiledPaths & kCurrentPath) != Path::kNone, BitNumber,
+        LhsScalar, RhsScalar, DstScalar, MulParamsType>::Search(the_path,
+                                                                params);
+  }
+};
+
+// Termination of the countdown. If the counter reaches -1, then we haven't
+// found the specified path.
+template <Path CompiledPaths, typename LhsScalar, typename RhsScalar,
+          typename DstScalar, typename MulParamsType>
+struct PathSearchCountdown<CompiledPaths, -1, LhsScalar, RhsScalar, DstScalar,
+                           MulParamsType> {
+  static void Search(Path, TrMulParams*) { RUY_DCHECK(false); }
+};
+
+template <Path CompiledPaths, typename LhsScalar, typename RhsScalar,
+          typename DstScalar, typename MulParamsType>
+void PopulateTrMulParamsAllCompiledPaths(Path the_path, TrMulParams* params) {
+  return PathSearchCountdown<CompiledPaths, 8 * sizeof(Path) - 1, LhsScalar,
+                             RhsScalar, DstScalar,
+                             MulParamsType>::Search(the_path, params);
+}
+
+}  // namespace detail
+
+// CreateTrMulParams is where we turn templatized ruy::Mul parameters to an
+// ordinary, un-templatized runtime data structure, TrMulParams. This is, in
+// particular, where we go from a template parameter 'CompiledPaths' describing
+// the set of paths enabled at compile-time, to runtime parameter values
+// corresponding to the specific choice of Path to be taken (encoded as function
+// pointers to kernel and pack code paths, and as runtime values of their
+// corresponding compile-time attributes e.g. the kernel-specific block layout).
+template <Path CompiledPaths, typename LhsScalar, typename RhsScalar,
+          typename DstScalar, typename MulParamsType>
+void CreateTrMulParams(const Mat<LhsScalar>& lhs, const Mat<RhsScalar>& rhs,
+                       const Mat<DstScalar>& dst,
+                       const MulParamsType& mul_params, Path the_path,
+                       TrMulParams* params) {
+  // Fill in the fields we already know.
+  params->src[Side::kLhs] = EraseType(lhs);
+  params->src[Side::kRhs] = EraseType(rhs);
+  params->dst = EraseType(dst);
+  params->mul_params = ToVoidPtr(&mul_params);
+
+  // Create inner loops and packed matrices based on the Path.
+  detail::PopulateTrMulParamsAllCompiledPaths<
+      CompiledPaths, LhsScalar, RhsScalar, DstScalar, MulParamsType>(the_path,
+                                                                     params);
+}
+
+}  // namespace ruy
+
+#endif  // RUY_RUY_CREATE_TRMUL_PARAMS_H_
---
+++ ruy/validate.h
@@ -0,0 +1,118 @@
+/* Copyright 2020 Google LLC. All Rights Reserved.
+
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+
+    http://www.apache.org/licenses/LICENSE-2.0
+
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
+==============================================================================*/
+
+// Front-end validation code, see the Validate function.
+
+#ifndef RUY_RUY_VALIDATE_H_
+#define RUY_RUY_VALIDATE_H_
+
+#include <cstdint>
+#include <limits>
+#include <type_traits>
+
+#include "ruy/check_macros.h"
+#include "ruy/common.h"
+#include "ruy/mat.h"
+#include "ruy/mul_params.h"
+#include "ruy/side_pair.h"
+
+namespace ruy {
+namespace detail {
+
+// If the MulParamsType's LayoutSupport covers only some special cases,
+// this function enforces that the matrix multiplication at hand falls into
+// that special case.
+template <typename MulParamsType>
+void ValidateLayouts(const MatLayout& lhs_layout, const MatLayout& rhs_layout,
+                     const MatLayout& dst_layout) {
+  if (MulParamsType::kLayoutSupport == LayoutSupport::kRCC) {
+    RUY_DCHECK(IsRowMajor(lhs_layout));
+    RUY_DCHECK(IsColMajor(rhs_layout));
+    RUY_DCHECK(IsColMajor(dst_layout));
+  }
+}
+
+template <typename Scalar>
+bool IsSymmetricZeroPoint(Scalar zero_point) {
+  return zero_point == SymmetricZeroPoint<Scalar>();
+}
+
+template <typename MulParamsType, typename Scalar>
+void CheckZeroPoint(Scalar zero_point) {
+  if (std::is_floating_point<Scalar>::value ||
+      MulParamsType::kZeroPointSupport == ZeroPointSupport::kSymmetric) {
+    RUY_DCHECK(IsSymmetricZeroPoint(zero_point));
+  }
+}
+
+template <typename MulParamsType, typename LhsScalar, typename RhsScalar,
+          typename DstScalar>
+void ValidateZeroPoints(LhsScalar lhs_zero_point, RhsScalar rhs_zero_point,
+                        DstScalar dst_zero_point) {
+  // If the MulParamsType's ZeroPointSupport covers only some special cases,
+  // this function enforces that the matrix multiplication at hand falls into
+  // that special case.
+  CheckZeroPoint<MulParamsType>(lhs_zero_point);
+  CheckZeroPoint<MulParamsType>(rhs_zero_point);
+  CheckZeroPoint<MulParamsType>(dst_zero_point);
+
+  // Guard against the case when both LHS and RHS zero_point's are equal to
+  // the minimum representable value. In that case, padding with zero_point
+  // values will generate the bad case for fast int8 kernels on NEON
+  // (pre-dotprod) which attempt to multiply-accumulate two pairs of int8
+  // into a int16:  this is safe except in the bad case -128*-128 + -128*-128.
+  // See b/131609283. This only affects the kNeon path but we ban this for all
+  // paths in order for ruy to have the same supported parameter space
+  // on all paths.
+  RUY_DCHECK(lhs_zero_point != std::numeric_limits<LhsScalar>::lowest() ||
+             rhs_zero_point != std::numeric_limits<RhsScalar>::lowest());
+}
+
+template <typename MulParamsType, typename DstScalar>
+void ValidateRawAccumulatorsDst(const MulParamsType& mul_params,
+                                DstScalar dst_zero_point) {
+  static_assert(
+      std::is_same<typename MulParamsType::DstScalar, DstScalar>::value, "");
+  if (!std::is_same<typename MulParamsType::DstScalar, std::int32_t>::value)
+    return;
+
+  // If user is looking for the raw accumulator, zero_point and all the other
+  // dequantize fields don't make sense and should not be set.
+  RUY_DCHECK_EQ(dst_zero_point, 0);
+  RUY_DCHECK_EQ(mul_params.clamp_max(),
+                std::numeric_limits<std::int32_t>::max());
+  RUY_DCHECK_EQ(mul_params.clamp_min(),
+                std::numeric_limits<std::int32_t>::min());
+  RUY_DCHECK_EQ(mul_params.multiplier_fixedpoint(), 0);
+  RUY_DCHECK_EQ(mul_params.multiplier_exponent(), 0);
+  RUY_DCHECK_EQ(mul_params.multiplier_fixedpoint_perchannel(), nullptr);
+  RUY_DCHECK_EQ(mul_params.multiplier_exponent_perchannel(), nullptr);
+}
+
+}  // namespace detail
+
+template <typename LhsScalar, typename RhsScalar, typename DstScalar,
+          typename MulParamsType>
+void Validate(const Mat<LhsScalar>& lhs, const Mat<RhsScalar>& rhs,
+              const Mat<DstScalar>& dst, const MulParamsType& mul_params) {
+  detail::ValidateLayouts<MulParamsType>(lhs.layout, rhs.layout, dst.layout);
+  detail::ValidateZeroPoints<MulParamsType>(lhs.zero_point, rhs.zero_point,
+                                            dst.zero_point);
+  detail::ValidateRawAccumulatorsDst<MulParamsType>(mul_params, dst.zero_point);
+}
+
+}  // namespace ruy
+
+#endif  // RUY_RUY_VALIDATE_H_
