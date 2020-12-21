177062d283a4bb14b414bc2f101a2103aee4fb71
Benoit Jacob <benoitjacob@google.com>
2020-12-21 12:39:19 -0800

    Move the example out of the ruy/ruy directory, and add an example returning raw int32 accumulators.
    PiperOrigin-RevId: 348511323

--- example/example.cc
+++ example/example.cc
@@ -126,6 +126,31 @@ void ExampleMulInt8PerChannelQuantized(ruy::Context *context) {
   std::cout << "RHS:\n" << rhs;
   std::cout << "Result:\n" << dst << "\n";
 }
+void ExampleMulInt8PerChannelGetRawAccumulators(ruy::Context *context) {
+  const std::int8_t lhs_data[] = {1, 2, 3, 4};
+  const std::int8_t rhs_data[] = {1, 2, 3, 4};
+  std::int32_t dst_data[4];
+
+  ruy::Matrix<std::int8_t> lhs;
+  ruy::MakeSimpleLayout(2, 2, ruy::Order::kRowMajor, lhs.mutable_layout());
+  lhs.set_data(lhs_data);
+  ruy::Matrix<std::int8_t> rhs;
+  ruy::MakeSimpleLayout(2, 2, ruy::Order::kColMajor, rhs.mutable_layout());
+  rhs.set_data(rhs_data);
+  ruy::Matrix<std::int32_t> dst;
+  ruy::MakeSimpleLayout(2, 2, ruy::Order::kColMajor, dst.mutable_layout());
+  dst.set_data(dst_data);
+
+  // When Dst is int32, mul_params is unused.
+  ruy::MulParams<std::int32_t, std::int32_t> mul_params;
+  ruy::Mul(lhs, rhs, mul_params, context, &dst);
+
+  std::cout << "Example Mul, int8 quantized with per-channel multipliers, "
+               "returning raw int32 accumulators:\n";
+  std::cout << "LHS:\n" << lhs;
+  std::cout << "RHS:\n" << rhs;
+  std::cout << "Result:\n" << dst << "\n";
+}
 
 int main() {
   ruy::Context context;
@@ -133,4 +158,5 @@ int main() {
   ExampleMulFloatWithBiasAddAndClamp(&context);
   ExampleMulUint8AsymmetricQuantized(&context);
   ExampleMulInt8PerChannelQuantized(&context);
+  ExampleMulInt8PerChannelGetRawAccumulators(&context);
 }
